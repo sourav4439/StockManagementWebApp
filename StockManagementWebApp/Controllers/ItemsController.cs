1d3cb7cd9eb04c00dec04503f0ca4d36445daa19
SOURAV-PC <SOURAV-PC@DESKTOP-0J16SI0>
2018-10-29 00:55:31 +0600

    ADDItemController

--- StockManagementWebApp/Controllers/ItemsController.cs
+++ StockManagementWebApp/Controllers/ItemsController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Migrations;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -93,7 +94,32 @@ namespace StockManagementWebApp.Controllers
         [HttpPost]
         public ActionResult Edit(Item item)
         {
-            return View();
+            ViewBag.category = db.Categories.Select(category => new SelectListItem()
+            {
+                Value = category.Id.ToString(),
+                Text = category.Name
+            }).ToList();
+
+            db.Items.AddOrUpdate(item);
+            db.SaveChanges();
+
+
+            return RedirectToAction("Index");
+            
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            var item = db.Items.Find(id);           
+            return View(item);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            Item item = db.Items.Find(id);
+            if (item != null) db.Items.Remove(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
 
